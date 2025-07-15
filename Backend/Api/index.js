031a31858353d4e697c793a7c647857b2635b975
Ata Ul Mustafa <ataulmustafajani@gmail.com>
2025-07-15 16:52:17 +0500

    some changing for render

--- Backend/Api/index.js
+++ Backend/Api/index.js
@@ -34,10 +34,10 @@ app.use('/Api/auth', authRouter);
 app.use('/Api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/Frontend/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
