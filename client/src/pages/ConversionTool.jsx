55a9e7e26826bcb03d0f630893626d818b2c6fa7
Ata Ul Mustafa <ataulmustafajani@gmail.com>
2025-07-22 14:21:16 +0500

    one again margin change

--- client/src/pages/ConversionTool.jsx
+++ client/src/pages/ConversionTool.jsx
@@ -54,7 +54,7 @@ const ConversionTool = () => {
 
   return (
     <section className="bg-gradient-to-br from-blue-100 via-white to-purple-100 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 pt-20 ">
-      <div className="max-w-5xl mx-auto px-4 mt-14">
+      <div className="max-w-5xl mx-auto px-4 mt-12">
         {/* Heading & Language Toggle */}
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
           <motion.h1
@@ -122,7 +122,7 @@ const ConversionTool = () => {
       </div>
 
       {/* Footer */}
-      <div className="mt-65">
+      <div className="mt-64">
         <Footer />
       </div>
     </section>
