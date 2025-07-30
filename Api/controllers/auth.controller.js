f41b7d6b9fc15332e8455a72dac8a2e3de0db1f9
Ata Ul Mustafa <ataulmustafajani@gmail.com>
2025-07-30 09:49:15 +0500

    update in controllers

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' }); //update
     const { password: pass, ...userData } = validUser._doc;
 
     res
